a2330b8d9bd4aa3f27752b969002e049733984dc
fercansen <fercansen@gmail.com>
2020-07-14 23:33:17 +0300

    small circle moves up and stops when it hits rotating circle

---
+++ aa clone/Assets/Scripts/SmallCircleScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallCircleScript : MonoBehaviour
+{
+    Rigidbody2D physics;
+    public float speed;
+    bool collisionControl = false;
+
+    void Start()
+    {
+        physics = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (!collisionControl)
+        {
+            physics.MovePosition(physics.position + Vector2.up * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "RotatingCircleTag")
+        {
+            collisionControl = true;
+        }
+    }
+}
