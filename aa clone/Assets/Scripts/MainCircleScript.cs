65424190c9373b80c3d6659c3c4429046554805e
fercansen <fercansen@gmail.com>
2020-07-15 01:07:20 +0300

    Main circle created and it shoots small circles

---
+++ aa clone/Assets/Scripts/MainCircleScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCircleScript : MonoBehaviour
+{
+    public GameObject smallCircle;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            smallCircleSpawn();
+        }
+    }
+
+    void smallCircleSpawn()
+    {
+        Instantiate(smallCircle, transform.position, transform.rotation);
+    }
+}
