1948bd54ee4198eaf5ef882127e75e6880217c6e
fercansen <fercansen@gmail.com>
2020-07-15 02:34:41 +0300

    Game Stops on collision with small circles

---
+++ aa clone/Assets/Scripts/CheckGameOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGameOver : MonoBehaviour
+{
+    GameObject rotatingCircle;
+    GameObject mainCircle;
+    void Start()
+    {
+        rotatingCircle = GameObject.FindGameObjectWithTag("RotatingCircleTag");
+        mainCircle = GameObject.FindGameObjectWithTag("MainCircleTag");
+    }
+
+    public void GameOver()
+    {
+        rotatingCircle.GetComponent<Rotate>().enabled = false;
+        mainCircle.GetComponent<MainCircleScript>().enabled = false;
+    }
+}
--- aa clone/Assets/Scripts/SmallCircleScript.cs
+++ aa clone/Assets/Scripts/SmallCircleScript.cs
@@ -7,10 +7,12 @@ public class SmallCircleScript : MonoBehaviour
     Rigidbody2D physics;
     public float speed;
     bool collisionControl = false;
+    GameObject gameOverObject;
 
     void Start()
     {
         physics = GetComponent<Rigidbody2D>();
+        gameOverObject = GameObject.FindGameObjectWithTag("GameOverTag");
     }
 
 
@@ -24,10 +26,14 @@ public class SmallCircleScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "RotatingCircleTag")
+        if (collision.CompareTag("RotatingCircleTag"))
         {
             transform.SetParent(collision.transform);
             collisionControl = true;
+        }
+        if (collision.CompareTag("SmallCircleTag"))
+        {
+            gameOverObject.GetComponent<CheckGameOver>().GameOver();
         }
     }
 }
