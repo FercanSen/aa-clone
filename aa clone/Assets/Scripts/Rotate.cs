b18fd91d01e64b82900eef2acdd50049e3290107
fercansen <fercansen@gmail.com>
2020-07-24 01:29:26 +0300

    Rotation direction changed

--- aa clone/Assets/Scripts/Rotate.cs
+++ aa clone/Assets/Scripts/Rotate.cs
@@ -1,14 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rotate : MonoBehaviour
 {
     public float speed;
 
+    
+
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, speed * Time.deltaTime);
+
+        float angle = transform.localEulerAngles.z;
+        angle = (angle > 180) ? angle - 360 : angle;
+
+        if (int.Parse(SceneManager.GetActiveScene().name) < 5)
+        {
+            //Debug.Log("Level is less than five.");
+            transform.Rotate(0, 0, speed * Time.deltaTime);
+        }
+        if (SceneManager.GetActiveScene().name == "5")
+        {
+            //Debug.Log("Level is five.");
+            transform.Rotate(0, 0, -speed * Time.deltaTime);
+        }
     }
 }
