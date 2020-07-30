d114b93024caff347c974aa901f5ab136cea2194
fercansen <fercansen@gmail.com>
2020-07-31 02:19:54 +0300

    Scale script created for levels 7 and 8

---
+++ aa clone/Assets/Scripts/Scale.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scale : MonoBehaviour
+{
+
+    Vector3 scaleChange = new Vector3(0.05f, 0.05f, 0.05f);
+
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "7")
+        {
+            transform.localScale += scaleChange;
+            if (transform.localScale.x >= 17)
+            {
+                // Since all x, y and z values of the scale will be same. We can just check one. I picked x randomly.
+                scaleChange = -scaleChange;
+            }
+            if (transform.localScale.x <= 8)
+            {
+                scaleChange = -scaleChange;
+            }
+        }
+
+        if (SceneManager.GetActiveScene().name == "8")
+        {
+
+            transform.localScale += scaleChange * 2;
+            if (transform.localScale.x >= 17)
+            {
+                // Since all x, y and z values of the scale will be same. We can just check one. I picked x randomly.
+                scaleChange = -scaleChange;
+            }
+            if (transform.localScale.x <= 8)
+            {
+                scaleChange = -scaleChange;
+            }
+        }
+    }
+}
