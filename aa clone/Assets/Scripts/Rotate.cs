24ffaacd0d7f11a92b77a27e9826aa16280ea14a
fercansen <fercansen@gmail.com>
2020-07-31 02:19:32 +0300

    Added comments in Rotate.cs for later possible use

--- aa clone/Assets/Scripts/Rotate.cs
+++ aa clone/Assets/Scripts/Rotate.cs
@@ -6,18 +6,56 @@ using UnityEngine.SceneManagement;
 public class Rotate : MonoBehaviour
 {
     public float speed;
+    GameObject rotatingCircle;
+    Vector3 scaleChange = new Vector3(0.01f, 0.01f, 0.01f);
+    //bool isRotationClockwise = true;
+
+    //private void Start()
+    //{
+    //    if (SceneManager.GetActiveScene().name == "7" || SceneManager.GetActiveScene().name == "8")
+    //    {
+    //        InvokeRepeating("ChangeRotation", 5, 5);
+    //        StartCoroutine(RotationChange());
+    //    }
+    //}
+
 
     void Update()
     {
-        if (int.Parse(SceneManager.GetActiveScene().name) <= 4 || (int.Parse(SceneManager.GetActiveScene().name) >= 7 && int.Parse(SceneManager.GetActiveScene().name) < 9))
+        if (int.Parse(SceneManager.GetActiveScene().name) <= 4)
         {
-            // Levels that are smaller than 4 and also level 7 and 8 rotates clockwise.
+            // Levels that are smaller than 4 rotates counter clockwise.
             transform.Rotate(0, 0, speed * Time.deltaTime);
         }
-        if ((int.Parse(SceneManager.GetActiveScene().name) >= 5 && int.Parse(SceneManager.GetActiveScene().name) < 7) || int.Parse(SceneManager.GetActiveScene().name) >= 9)
+        if (SceneManager.GetActiveScene().name == "5" || SceneManager.GetActiveScene().name == "6")
         {
-            // Levels that are bigger than 5 and also level 8 and 9 rotates counter clockwise.
+            // Levels that are bigger than 5 rotates clockwise.
             transform.Rotate(0, 0, -speed * Time.deltaTime);
         }
+
+        if (SceneManager.GetActiveScene().name == "7" || SceneManager.GetActiveScene().name == "8")
+        {
+            transform.Rotate(0, 0, -speed * Time.deltaTime);
+
+            //if (isRotationClockwise)
+            //{
+            //    StartCoroutine(RotationChange());
+            //}
+        }
     }
+
+    //void ChangeRotation()
+    //{
+    //    Debug.Log("Change rotation works.");
+    //    transform.Rotate(0, 0, speed * Time.deltaTime);
+    //}
+
+    //IEnumerator RotationChange()
+    //{
+
+    //    Debug.Log("Coroutine works.");
+    //    transform.Rotate(0, 0, speed * Time.deltaTime);
+    //    isRotationClockwise = false;
+    //    yield return null;
+    //}
 }
